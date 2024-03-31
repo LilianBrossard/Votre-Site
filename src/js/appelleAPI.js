3b8e97b2570e9613e962c263d15c1a5e64aa4d8a
librossard <lilian.brossard@etudiant.univ-rennes1.fr>
2024-03-31 01:16:42 +0100

    v0.1.3

--- src/js/appelleAPI.js
+++ src/js/appelleAPI.js
@@ -60,3 +60,56 @@ export async function actu() {
     const actualites = actualitesData.data.actualites;
     return actualites;
 }
+export async function stat() {
+    const hygraphEndpoint = import.meta.env.HYGRAPH_ENDPOINT;
+    const response = await fetch(hygraphEndpoint, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        query: `
+            {
+            stats(orderBy: id_ASC) {
+                nomDeLaStat
+                statEnQuestion
+                image {
+                height
+                url
+                width
+                }
+            }
+            }`,
+    }),
+    });
+    const statsData = await response.json();
+    const stats = statsData.data.stats;
+    return stats;
+}
+export async function poste() {
+    const hygraphEndpoint = import.meta.env.HYGRAPH_ENDPOINT;
+    const response = await fetch(hygraphEndpoint, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        query: `
+            {
+            postes(orderBy: publishedAt_ASC) {
+                nomDuPoste
+                descriptionDuPoste {
+                    html
+                }
+                resumeDuPoste
+                    slug
+                }
+            }`,
+    }),
+    });
+    const postesData = await response.json();
+    const postes = postesData.data.postes;
+    return postes;
+}
